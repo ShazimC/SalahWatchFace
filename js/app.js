4e8a66870b94be5f82da20af392db5d64601915d
ShazimC <shc77@scarletmail.rutgers.edu>
2021-09-27 21:48:59 -0400

    Flexbox styling

--- js/app.js
+++ js/app.js
@@ -16,31 +16,11 @@
 
 (function () {
     /**
-     * ARR_COLOR - Color string array for displaying air pollution and battery level.
-     * ARR_MONTH - Month string array for displaying month-text.
-     * URL_WEATHER_DATA - Address of file contains weather information.
-     * URL_AIR_POLLUTION_DATA - Address of file contains air pollution information.
      * battery - Object contains the devices's battery information.
      * updateTimer - Object contains date update timer
      */
-    var ARR_COLOR = ["red", "orange", "yellow", "green", "blue"],
-        ARR_MONTH = ["JANUARY", "FEBRUARY", "MARCH", "APRIL", "MAY", "JUNE", "JULY", "AUGUST", "SEPTEMBER", "OCTOBER", "NOVEMBER", "DECEMBER"],
-        //If you want to get weather data from another file or API, change this part.
-        URL_WEATHER_DATA = "./data/weatherData.json",
-        //If you want to get air pollution data from another file or API, change this part.
-        URL_AIR_POLLUTION_DATA = "./data/airPollutionData.xml",
-        battery = navigator.battery || navigator.webkitBattery || navigator.mozBattery,
+    var battery = navigator.battery || navigator.webkitBattery || navigator.mozBattery,
         updateTimer;
-
-    /**
-     * Rotates element.
-     * @private
-     * @param {object} element - The object will be rotated
-     * @param {number} batteryGraderotateAngle - The angle for rotating
-     */
-    function rotateElement(element, rotateAngle) {
-        element.style.transform = "rotate(" + rotateAngle + "deg)";
-    }
 
     /**
      * Updates the current time.
@@ -48,16 +28,6 @@
      */
     function updateTime() {
         var dateTime = tizen.time.getCurrentDateTime();
-        // second = dateTime.getSeconds(),
-        // minute = dateTime.getMinutes(),
-        // hour = dateTime.getHours(),
-        // elSecIndicator = document.querySelector("#time-second-indicator"),
-        // elMinIndicator = document.querySelector("#time-min-indicator"),
-        // elHourIndicator = document.querySelector("#time-hour-indicator");
-
-        // rotateElement(elSecIndicator, (second * 6));
-        // rotateElement(elMinIndicator, (minute + (second / 60)) * 6);
-        // rotateElement(elHourIndicator, (hour + (minute / 60) + (second / 3600)) * 30);
         var timeIndicator = document.querySelector("#formatted-time");
         timeIndicator.innerHTML = dateTime.toLocaleTimeString();
     }
@@ -69,10 +39,8 @@
      */
     function updateDate(prevDate) {
         var dateTime = tizen.time.getCurrentDateTime(),
-            month = dateTime.getMonth(),
             date = dateTime.getDate(),
-            elMonthStr = document.querySelector("#calendar-month"),
-            elDateStr = document.querySelector("#calendar-date"),
+            dateText = document.querySelector('#date-text'),
             nextInterval;
 
         /**
@@ -99,9 +67,9 @@
                     1;
             }
         }
-
-        elMonthStr.innerHTML = ARR_MONTH[month];
-        elDateStr.innerHTML = date;
+        var dateString = dateTime.toLocaleDateString();
+        var lastComma = dateString.lastIndexOf(',');
+        dateText.innerHTML = dateString.substring(0, lastComma);
 
         // If an updateDate timer already exists, clear the previous timer.
         if (updateTimer) {
@@ -114,38 +82,29 @@
         }, nextInterval);
     }
 
-    /**
-     * When fail to get location data or weather, air pollution data,
-     * sets disable icon (GPS or WiFi).
-     * @private
-     * @param {string} iconName - The icon name for displaying
-     */
-    function setDisableIcon(iconName) {
-        var elAirPollIcon = document.querySelector("#air-icon"),
-            elAirPollStatus = document.querySelector("#air-status"),
-            elAirPollText = document.querySelector("#air-text");
-
-        elAirPollStatus.style.backgroundImage = "url('./image/no_" + iconName + "_icon.png')";
-        elAirPollIcon.style.backgroundImage = "";
-        elAirPollText.innerHTML = "";
+    function addParamsToUrl(url = '', queryParams = {}){
+        var urlWithParams = url+'?'; 
+        for (var param in queryParams) {
+            var value = queryParams[param];
+            var paramString = param + '=' + value + '&';
+            urlWithParams += paramString;
+        }
+        return urlWithParams;
     }
 
     function updateSalah({ lat, long }) {
         var salahText = document.querySelector('#salah-status');
+        salahText.innerHTML = "Updating..";
+
         var req = new XMLHttpRequest();
-        var baseURL = 'http://api.aladhan.com/v1/calendar';
-        var queryParams = {
+        var apiURL = addParamsToUrl('http://api.aladhan.com/v1/calendar', {
             latitude: lat,
             longitude: long,
             school: 1
-        }
-        var urlWithParams = baseURL+'?';
-        for (var param in queryParams) {
-            var value = queryParams[param];
-            urlWithParams += `${param}=${value}&`;
-        }
+        });
+
         req.overrideMimeType("application/json");
-        req.open("GET", urlWithParams, false);
+        req.open("GET", apiURL, false);
         req.onreadystatechange = function () {
             if (req.responseText) {
                 var res = JSON.parse(req.responseText)
@@ -164,15 +123,17 @@
         var output = 'error';
         if(timings && typeof timings === "object"){
             for(var salah in timings){
+                if(salah === 'Sunset') continue;
                 var unparsed = timings[salah];
                 var spaceIndex = unparsed.indexOf(' ');
-                var timeOnly = unparsed.substring(0, spaceIndex);
-                var colonIndex = timeOnly.indexOf(':');
-                var hour = timeOnly.substring(0, colonIndex);
-                var mins = timeOnly.substring(colonIndex + 1);
+                var timeStr = unparsed.substring(0, spaceIndex);
+                var colonIndex = timeStr.indexOf(':');
+                var hour = timeStr.substring(0, colonIndex);
+                var mins = timeStr.substring(colonIndex + 1);
                 var salahTime = new Date();
                 salahTime.setHours(hour, mins);
-                output = salah + ' - ' + salahTime.getHours() + ':' + salahTime.getMinutes();
+                var formattedTime = formatTime(salahTime);
+                output = salah + ' - ' + formattedTime;
                 if(now.getHours() < salahTime.getHours()){
                     return output;
                 }else if(now.getHours() === salahTime.getHours()){
@@ -185,99 +146,22 @@
         return output;
     }
 
-    /**
-     * Updates weather icon, status and text.
-     * @private
-     */
-    function updateWeather() {
-        /**
-         * xmlHttp - XMLHttpRequest object for get information about weather
-         */
-        var xmlHttp = new XMLHttpRequest(),
-            weatherInform,
-            elWeatherIcon = document.querySelector("#weather-icon"),
-            elWeatherText = document.querySelector("#weather-text"),
-            weatherIcon,
-            weatherText;
-
-        xmlHttp.overrideMimeType("application/json");
-        xmlHttp.open("GET", URL_WEATHER_DATA, false);
-        xmlHttp.onreadystatechange = function () {
-            // Checks responseText isn't empty
-            if (xmlHttp.responseText) {
-                // Parses responseText to JSON
-                weatherInform = JSON.parse(xmlHttp.responseText);
-                // Gets icon code from information
-                weatherIcon = weatherInform.weather[0].icon;
-                // Gets weather string from information
-                weatherText = weatherInform.weather[0].main;
-
-                elWeatherIcon.style.backgroundImage = "url('./image/weather_icon/" + weatherIcon + ".png')";
-                elWeatherText.innerHTML = weatherText;
-            }
-            // If reponseText is empty, set no wifi icon.
-            else {
-                setDisableIcon("wifi");
-            }
-        };
-
-        xmlHttp.send();
+    function formatTime(time = new Date()){
+        var timeStr = time.toLocaleTimeString();
+        var lastColon = timeStr.lastIndexOf(':');
+        var firstPart = timeStr.substring(0, lastColon);
+        var space = timeStr.indexOf(' ');
+        var secondPart = timeStr.substring(space);
+        return firstPart + secondPart;
     }
 
     /**
-     * Updates air pollution icon, status and text.
-     * @private
+     * Updates displayed weather temperature based on given geolocation coordinates.
+     * @param {{number}} lat the latitude of the geolocation.
+     * @param {{number}} long the longitude of the geolocation.
      */
-    function updateAirPolution() {
-        /**
-         * xmlHttp - XMLHttpRequest object for get information about air pollution
-         */
-        var xmlHttp = new XMLHttpRequest(),
-            airPollutionInform,
-            elAirPollIcon = document.querySelector("#air-icon"),
-            elAirPollStatus = document.querySelector("#air-status"),
-            elAirPollText = document.querySelector("#air-text"),
-            airPollLevel,
-            airPollGrade,
-            statusColor;
-
-        xmlHttp.open("GET", URL_AIR_POLLUTION_DATA, false);
-        xmlHttp.onreadystatechange = function () {
-            // Checks responseXML isn't empty
-            if (xmlHttp.responseXML) {
-                airPollutionInform = xmlHttp.responseXML;
-                // Gets air pollution level from pm10value node in responseXML
-                airPollLevel = airPollutionInform.getElementsByTagName("pm10Value")[0].childNodes[0].nodeValue;
-                elAirPollText.innerHTML = airPollLevel;
-
-                if (airPollLevel === "-") {
-                    airPollGrade = 4;
-                } else {
-                    elAirPollText.innerHTML += "AQI";
-                    if (airPollLevel < 50) {
-                        airPollGrade = 4;
-                    } else if (airPollLevel < 150) {
-                        airPollGrade = 3;
-                    } else if (airPollLevel < 200) {
-                        airPollGrade = 2;
-                    } else if (airPollLevel < 300) {
-                        airPollGrade = 1;
-                    } else {
-                        airPollGrade = 0;
-                    }
-                }
-
-                statusColor = ARR_COLOR[airPollGrade];
-                elAirPollIcon.style.backgroundImage = "url('./image/color_status/air_pollution_icon_" + statusColor + ".png')";
-                elAirPollStatus.style.backgroundImage = "url('./image/color_status/" + statusColor + "_indicator.png')";
-            }
-            // If reponseXML is empty, set no wifi icon.
-            else {
-                setDisableIcon("wifi");
-            }
-        };
-
-        xmlHttp.send();
+    function updateWeather({lat, long}) {
+        //make api request to some weather api using coordinates
     }
 
     /**
@@ -285,16 +169,10 @@
      * @private
      */
     function updateBattery() {
-        var elBatteryIcon = document.querySelector("#battery-icon"),
-            elBatteryStatus = document.querySelector("#battery-status"),
-            elBatteryText = document.querySelector("#battery-text"),
-            batteryLevel = Math.floor(battery.level * 100),
-            batteryGrade = Math.floor(batteryLevel / 20),
-            statusColor = ARR_COLOR[batteryGrade];
+        var batteryText = document.querySelector("#battery-text"),
+            batteryLevel = Math.floor(battery.level * 100);
 
-        elBatteryIcon.style.backgroundImage = "url('./image/color_status/battery_icon_" + statusColor + ".png')";
-        elBatteryStatus.style.backgroundImage = "url('./image/color_status/" + statusColor + "_indicator.png')";
-        elBatteryText.innerHTML = batteryLevel + "%";
+        batteryText.innerHTML = batteryLevel + "%";
     }
 
     /**
@@ -318,27 +196,12 @@
                     lat: pos.coords.latitude,
                     long: pos.coords.longitude
                 });
-                updateWeather();
-                updateAirPolution();
-            },
-            setDisableIcon("gps")
+                // updateWeather({
+                //     lat: pos.coords.latitude,
+                //     long: pos.coords.longitude
+                // });
+            }
         );
-    }
-
-    /**
-     * Changes display attribute of two elements when occur click event
-     * @private
-     * @param {object} element1 - The first element id for changing display
-     * @param {object} element2 - The second element id for changing display
-     */
-    function toggleElement(element1, element2) {
-        if (document.querySelector(element1).style.display === "none") {
-            document.querySelector(element1).style.display = "block";
-            document.querySelector(element2).style.display = "none";
-        } else {
-            document.querySelector(element1).style.display = "none";
-            document.querySelector(element2).style.display = "block";
-        }
     }
 
     /**
@@ -346,12 +209,6 @@
      * @private
      */
     function bindEvents() {
-        /**
-         * elBattery - Element contains battery icon, status and text
-         * elAir - Element contains air pollution icon, status and text
-         */
-        var elBattery = document.querySelector("#body-battery"),
-            elAir = document.querySelector("#body-air");
 
         // Adds eventListener to update the screen immediately when the device wakes up.
         document.addEventListener("visibilitychange", function () {
@@ -372,16 +229,6 @@
         battery.addEventListener("chargingtimechange", updateBattery);
         battery.addEventListener("dischargingtimechange", updateBattery);
         battery.addEventListener("levelchange", updateBattery);
-
-        // Adds event listeners to change displaying child element when the battery element is clicked.
-        elBattery.addEventListener("click", function () {
-            toggleElement("#battery-icon", "#battery-text");
-        });
-
-        // Adds event listeners to change displaying child element when the air pollution element is clicked.
-        elAir.addEventListener("click", function () {
-            toggleElement("#air-icon", "#air-text");
-        });
     }
 
     /**
